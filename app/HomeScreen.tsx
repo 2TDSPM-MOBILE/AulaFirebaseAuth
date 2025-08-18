cb04363d4581bc7d97995e80f0a35a0d431f6a0f
Prof. Fernando Pinéo <pf1963@fiap.com.br>
2025-08-18 12:09:00 -0300

    Realizado o botão para excluir a senha

--- app/HomeScreen.tsx
+++ app/HomeScreen.tsx
@@ -1,7 +1,10 @@
-import { Text,Button } from "react-native";
+import { Text,Button,Alert } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useRouter } from "expo-router";
+
+import {auth} from '../services/firebaseConfig'
+import { deleteUser } from "firebase/auth";
 
 
 export default function HomeScreen(){
@@ -9,13 +12,42 @@ export default function HomeScreen(){
 
     const realizarLogoff = async()=>{
         await AsyncStorage.removeItem('@user')
-        router.push('./')//Redireciona para index.tsx
+        router.push('/')//Redireciona para index.tsx
+    }
+
+    const excluirConta = ()=>{
+        Alert.alert("CONFIRMAR EXCLUSÃO","Tem certeza que deseja excluir? Esta ação não poderá ser desfeita.",
+            [
+                {text:'Cancelar', style:'cancel'},
+                {text:'Excluir', style:'destructive',
+                    onPress:async()=>{
+                        try{
+                            const user = auth.currentUser
+                            if(user){
+                                await deleteUser(user)//Deleta o user do firebase Auth
+                                await AsyncStorage.removeItem('@user')
+                                Alert.alert("Conta Excluída","Sua conta foi excluída com sucesso.")
+                                router.replace('/')
+                            }else{
+                                Alert.alert("Erro","Nenhum usuário logado")
+                            }
+                        }catch(error){
+                            console.log("Erro ao excluir conta")
+                            Alert.alert("Erro","Não foi possível excluir a conta")
+                        }
+                    }
+                }
+            ],{
+                cancelable:true
+            }
+        )
     }
 
     return(
         <SafeAreaView>
             <Text>Seja bem-vindo - Você está Logado!!</Text>
             <Button title="Sair da conta" onPress={realizarLogoff}/>
+            <Button title="Excluir conta" color='red' onPress={excluirConta}/>
         </SafeAreaView>
     )
 }
